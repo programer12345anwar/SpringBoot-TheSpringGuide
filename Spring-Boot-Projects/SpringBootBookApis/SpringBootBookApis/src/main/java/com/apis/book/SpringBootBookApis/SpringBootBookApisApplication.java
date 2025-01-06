3db3cd26a15bead51862fe17a396a80be55f67dd
programer12345anwar <mdanwar40212@gmail.com>
2025-01-06 15:40:50 +0530

    second commit

---
+++ Spring-Boot-Projects/SpringBootBookApis/SpringBootBookApis/src/main/java/com/apis/book/SpringBootBookApis/SpringBootBookApisApplication.java
@@ -0,0 +1,13 @@
+package com.apis.book.SpringBootBookApis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBookApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBookApisApplication.class, args);
+	}
+
+}
---
+++ Spring-Boot-Projects/SpringBootBookApis/SpringBootBookApis/src/test/java/com/apis/book/SpringBootBookApis/SpringBootBookApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apis.book.SpringBootBookApis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBookApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
